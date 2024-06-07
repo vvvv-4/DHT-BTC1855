28fd4114e41f457516be934fabaaecb92e18a3e3
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-06-07 18:09:57 -0400

    Initial commit

---
+++ push_test.R
@@ -0,0 +1,16 @@
+#' This is a test script
+#' We will run some little functions
+#' Then save this as push_test.R
+#' 
+
+# it would have been more professional to use parameter names 
+myvector <- seq(100, 1, by=-7)
+
+myavg <- mean(myvector)
+myavg
+
+mysum <- sum(myvector)
+mysum
+
+mysd <- sd(myvector)
+mysd
