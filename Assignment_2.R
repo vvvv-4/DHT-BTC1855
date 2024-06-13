a86aeebabf5e3af20bcfa283743fd60f579b78e3
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-06-13 17:09:23 -0400

    Started question 1

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,1 +1,20 @@
-# Assignment 2
+# Mikael Gouwtama
+# 1007128127
+# BTC1855 Assignment 2
+
+# Prompt the user to enter a three digit positive number
+number <- readline(prompt="Enter a three digit positive number: ")
+
+if (is.na(as.numeric(number))) {
+  print(paste(number, "is not numeric"))
+  stop()
+}
+
+
+
+if (is.numeric(number)) {
+  print(paste(number, "is numeric"))
+} else if (!is.numeric(number)) {
+  print(paste(number, "is not numeric"))
+  stop()
+}
