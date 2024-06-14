995b655d366719cb6870cc6e25d3aa7432986bb0
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-06-14 09:02:53 -0400

    Added some else condition when a three digit positive number is printed to answer #2

--- Assignment_2.R
+++ Assignment_2.R
@@ -5,8 +5,10 @@
 # Prompt the user to enter a three digit positive number
 number <- readline(prompt="Enter a three digit positive number: ")
 
-# Check if the user input is numeric. If not, print an error message and quit.
+# Check if the user input is numeric. If not, print an error message and quit. 
 if (is.na(as.numeric(number)) || nchar(number) != 3 || as.numeric(number) <= 0) {
   stop("The input is either not numeric, not a three digit number, or not a positive number")
+} else {
+  print(paste("You have entered a three digit positive number: ", number))
 }
 
