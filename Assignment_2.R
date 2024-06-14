b803b842c69f2bbddf0dac6ba805804d1a3bef78
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-06-14 09:13:20 -0400

    Changing the number variable to input for clarity

--- Assignment_2.R
+++ Assignment_2.R
@@ -3,12 +3,15 @@
 # BTC1855 Assignment 2
 
 # Prompt the user to enter a three digit positive number
-number <- readline(prompt="Enter a three digit positive number: ")
+input <- readline(prompt="Enter a three digit positive number: ")
 
 # Check if the user input is numeric. If not, print an error message and quit. 
-if (is.na(as.numeric(number)) || nchar(number) != 3 || as.numeric(number) <= 0) {
+if (is.na(as.numeric(input)) || nchar(input) != 3 || as.numeric(input) <= 0) {
   stop("The input is either not numeric, not a three digit number, or not a positive number")
 } else {
-  print(paste("You have entered a three digit positive number: ", number))
+  print(paste("You have entered a three digit positive number: ", input))
 }
 
+# Checking if the input is narcissistic
+# Get the first, second, and third numbers from the user's input using the substr() function
+first_numr <- substr()
