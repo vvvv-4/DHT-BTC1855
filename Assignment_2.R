c4028c1ff26ff3540b97a752065b0b97a728f94d
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-06-17 22:29:52 -0400

    Completed final if conditions

--- Assignment_2.R
+++ Assignment_2.R
@@ -29,34 +29,13 @@ if (is.na(input_num)){
   
   sum_cubes - (first_num)^3 + (second_num)^3 + (third_num)^3
   
+  if (sum_cubes == as.numeric(input)){
+    # If the sum of the cubes is the same as the input, print it is a 
+    # narcissistic number
+    print(paste(input, "is a narcissistic number"))
+  } else {
+    # If the sum of the cube is not the same as the input, print it as 
+    # its is not an Armstrong number
+    print(paste(input, "is not an Armstrong number"))
+  }
 }
-
-
-# If the sum of the cubes is the same as the input, print it is a narcissistic number
-# If the sum of the cube is not the same as the input, print it is not an Armstrong number
-
-
-# Check if the user input is numeric. If not, print an error message and quit. 
-if (is.na(as.numeric(input)) || nchar(input) != 3 || as.numeric(input) <= 0) {
-  print("The input is either not numeric, not a three digit number, or not a positive number")
-} else {
-  print(paste("You have entered a three digit positive number: ", input))
-}
-
-# Checking if the input is narcissistic
-# Get the first, second, and third numbers from the user's input using the 
-# substr() function and make that number numeric
-first_num <- as.numeric(substr(input, 1, 1))
-second_num <- as.numeric(substr(input, 2, 2))
-third_num <- as.numeric(substr(input, 3, 3))
-
-# Get the sum of the cubes of the first three numbers
-sum_cubes <- (first_num)^3 + (second_num)^3 + (third_num)^3
-
-# Finally, check if the sum_cubes is the same as the original input
-if (sum_cubes == as.numeric(input)){
-  print(paste(input, "is a narcissistic number"))
-} else {
-  print(paste(input, "is not an Armstrong number"))
-}
-
