f94ddf0f5ec9de90e14ccc415a14766f6d0dba00
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-07-01 23:09:08 -0400

    Added some comments

--- Assignment_3/Assignment_3.R
+++ Assignment_3/Assignment_3.R
@@ -5,36 +5,37 @@
 # Assuming that the words_list file is in the same directory as this file
 # Get the list of words from the words_list.txt file and make it into a vector
 words <- readLines("words_list.txt")
-#words
-
-# other method to read the text file
-#words2 <- read.delim("words_list.txt", header = FALSE)
-#words2
-#words2[[1]][1]
 
 # Randomly select 1 word from the list of words
 answer_key <- sample(words, 1)
 answer_key <- unlist(strsplit(answer_key, ""))
 
+# Provide user the length of the answer
 print(paste("The length of the answer word is", length(answer_key)))
 
 # Initiate and report the number of tries the user has
+# Here, we set the number of tries the user have as 5. 
 tries <- 5
 print(paste0("Guess this word. The number of tries you have is ", tries, ". Good Luck!"))
+
+# Create a display of the user's correct answer so far. This will begin as 
+# underscores corresponding to the length of the answer key
 user_answer <- rep("_", length(answer_key))
 
 # Initialize an empty vector for the correct guesses
 your_guesses <- vector() 
 
-user_input <- function(){
+user_input <- function(your_guesses){
   # Want the question to be asked as least once
   repeat{
     input <- readline(prompt = "Enter a single letter: ")
     # check if the input is a letter and a single character
-    if (nchar(input) == 1 & grepl("^[a-zA-Z]$", input)){
+    if (input %in% your_guesses){
+      cat("You have already guessed this. Please enter a different letter.", "\n")
+    } else if (nchar(input) == 1 & grepl("^[a-zA-Z]$", input)) {
       break
     } else {
-      cat("Input is invalid. Please enter a single letter")
+      cat("Input is invalid. Please enter a single letter", "\n")
     }
   }
   return(tolower(input))
@@ -47,9 +48,9 @@ while (tries != 0) {
   cat("remaining tries: ", tries, "\n")
   
   # get input from user
-  current <- user_input()
+  current <- user_input(your_guesses)
   
-  your_guesses <- c(your_guesses, current)
+  your_guesses <- sort(c(your_guesses, current))
   
   if (current %in% answer_key) {
     cat("Nice, you have guessed a letter correctly!", "\n")
@@ -59,13 +60,13 @@ while (tries != 0) {
     user_answer[positions] <- current
     
     if (identical(user_answer, answer_key)) {
-      cat("Congratulation!, you have guessed the word correctly:", paste(answer_key, collapse = ""))
+      cat("Congratulations! You have guessed the word correctly:", paste(answer_key, collapse = ""))
       break
     }
     
   } else {
     tries = tries - 1
-    cat("Oops, incorrect guess. You now have", tries, "tries remaining", "\n")
+    cat("Oops, incorrect guess.", "\n")
   }
 }
 
