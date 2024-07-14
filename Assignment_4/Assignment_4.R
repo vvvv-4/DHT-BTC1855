d8229eed630f417bd1ea91d20bb81660346a4cc1
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-07-14 17:14:15 -0400

    Checking the structure of the columns and modify some of it

--- Assignment_4/Assignment_4.R
+++ Assignment_4/Assignment_4.R
@@ -2,6 +2,28 @@
 # 1007128127
 # BTC1855 Assignment 4
 
-# added some test
+# Read the ufo_subset data into a data frame
+ufo <- read.csv("ufo_subset.csv")
 
-# more tests
+class(ufo)
+
+# check and correct structural issue
+
+str(ufo)
+
+summary(ufo)
+
+## convert datetime into date 
+library(lubridate)
+
+ufo$datetime <- ymd_hm(ufo$datetime)
+
+str(ufo)
+
+## convert date_posted into date
+
+ufo$date_posted <- dmy(ufo$date_posted)
+
+str(ufo)
+
+ufo$date_posted <- ymd(ufo$date_posted)
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Hello! Welcome to Mikael Gouwtama's BTC1855 assignment page
 
 You can find the assignments file from here
 
-The most recent upload is assignment 3. A hangman game! Enjoy!!
+The most recent upload is assignment 4. Mars?!
