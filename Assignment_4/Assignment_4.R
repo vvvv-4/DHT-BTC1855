a4bea2d2d0a68ca01bc581f89bb40afb84f54eb7
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-07-17 16:53:47 -0400

    standardization and creating original copy

--- Assignment_4/Assignment_4.R
+++ Assignment_4/Assignment_4.R
@@ -14,122 +14,102 @@ str(ufo)
 
 summary(ufo)
 
+# Create a copy of the orignial dataset
+ufo_work <- ufo
+
 ## convert datetime into date 
 library(lubridate)
 
-ufo$datetime <- ymd_hm(ufo$datetime)
+ufo_work$datetime <- ymd_hm(ufo_work$datetime)
 
-str(ufo$datetime)
+str(ufo_work$datetime)
 
 ## convert date_posted into date
 
-ufo$date_posted <- dmy(ufo$date_posted)
+ufo_work$date_posted <- dmy(ufo_work$date_posted)
 
-str(ufo$date_posted)
+str(ufo_work$date_posted)
 
 # converting date_posted into posixct
-ufo$date_posted <- as.POSIXct(ufo$date_posted)
+ufo_work$date_posted <- as.POSIXct(ufo_work$date_posted)
 
-str(ufo$date_posted)
+str(ufo_work$date_posted)
 
 ## change the duration.hours.min columns so that they report the hours and minutes based on the duration.seconds and make it consistent
 
-ufo <- ufo %>%
+ufo_work <- ufo_work %>%
   mutate(duration.hours.min = paste(floor(duration.seconds / 3600), 
                                     floor((duration.seconds %% 3600) / 60), 
                                     sep = ":"))
 
-str(ufo)
+str(ufo_work)
 
 # Identify and remove duplicate entries
-ufo_no_dupe <- ufo %>%
+ufo_work <- ufo_work %>%
   distinct()
 
 # Remove rows with missing `country` values
 # It seems like seconds 0.02 is problematic by looking at the comments
-ufo_no_dupe <- ufo_no_dupe %>%
+ufo_work <- ufo_work %>%
   filter(!is.na(country) & country != "") %>%
   filter(!is.na(shape) & shape != "") %>%
   filter(duration.seconds != 0.02)
 
-str(ufo_no_dupe)
+str(ufo_work)
 
-summary(ufo_no_dupe)
+summary(ufo_work)
 
 
 # Check for missing values in 'country', 'shape', and 'duration.seconds'
-sum(is.na(ufo_no_dupe$country))
-sum(is.na(ufo_no_dupe$shape))
-sum(is.na(ufo_no_dupe$`duration seconds`))
+sum(is.na(ufo_work$country))
+sum(is.na(ufo_work$shape))
+sum(is.na(ufo_work$duration.seconds))
 
 # Look at the distribution in duration.seconds using boxplot
-boxplot(ufo_no_dupe$duration.seconds)
+boxplot(ufo_work$duration.seconds)
 
-head(sort(ufo_no_dupe$duration.seconds, decreasing = T), 10)
+head(sort(ufo_work$duration.seconds, decreasing = T), 10)
 
 # Taking only the 99th percentile
-threshold <- quantile(ufo_no_dupe$duration.seconds, 0.99)
-ufo_no_dupe <- ufo_no_dupe %>%
+threshold <- quantile(ufo_work$duration.seconds, 0.99)
+ufo_work <- ufo_work %>%
   filter(duration.seconds <= threshold)
 
 # Visualize the distribution of duration.seconds
-hist(ufo_no_dupe$duration.seconds, breaks = 100, main = "Histogram of Duration Seconds", xlab = "Duration Seconds")
+hist(ufo_work$duration.seconds, breaks = 100, main = "Histogram of Duration Seconds", xlab = "Duration Seconds")
 
 # Examine values at different percentiles
-quantile(ufo_no_dupe$duration.seconds, probs = seq(0.9, 1, by = 0.01))
+quantile(ufo_work$duration.seconds, probs = seq(0.9, 1, by = 0.01))
 
 # Taking only the 90th percentile
-threshold <- quantile(ufo_no_dupe$duration.seconds, 0.90)
-ufo_no_dupe <- ufo_no_dupe %>%
+threshold <- quantile(ufo_work$duration.seconds, 0.90)
+ufo_work <- ufo_work %>%
   filter(duration.seconds <= threshold)
 
-hist(ufo_no_dupe$duration.seconds, breaks = 100, main = "Histogram of Duration Seconds", xlab = "Duration Seconds")
+hist(ufo_work$duration.seconds, breaks = 100, main = "Histogram of Duration Seconds", xlab = "Duration Seconds")
 
-boxplot(ufo_no_dupe$duration.seconds)
+boxplot(ufo_work$duration.seconds)
 
 
 # Now, look at the summaries for the three variables of interest
+summary(ufo_work$country)
+summary(ufo_work$shape)
+summary(ufo_work$duration.seconds)
 
-summary(ufo_no_dupe$country)
-summary(ufo_no_dupe$shape)
-summary(ufo_no_dupe$duration.seconds)
+table(ufo_work$country, useNA = "ifany")
 
-table(ufo_no_dupe$country, useNA = "ifany")
-
-table(ufo_no_dupe$shape, useNA = "ifany")
-
-
+table(ufo_work$shape, useNA = "ifany")
 
 # Standardize 'country' and 'shape' values to handle inconsistencies
-ufo_no_dupe$country <- toupper(ufo_no_dupe$country)
-ufo_no_dupe$shape <- tolower(ufo_no_dupe$shape)
+ufo_work$country <- toupper(ufo_work$country)
+ufo_work$shape <- tolower(ufo_work$shape)
 
 # Recheck frequency tables for 'country' and 'shape'
-table(ufo_no_dupe$country, useNA = "ifany")
-table(ufo_no_dupe$shape, useNA = "ifany")
+table(ufo_work$country, useNA = "ifany")
+table(ufo_work$shape, useNA = "ifany")
 
 
 
-
-
-# Remove rows with missing '
-
-#sum(ufo_no_dupe$shape == "")
-
-#sum(is.na(ufo_no_dupe$shape))
-
-#sum(ufo_no_dupe$duration.seconds <= 0.02)
-
-
-
-#sum(is.na(ufo_no_dupe$duration.seconds))
-
-# identify weird data
-#summary(ufo_no_dupe)
-
-#which(ufo$duration.seconds == 82800000)
-
-#ufo[17253, ]
 
 
 
